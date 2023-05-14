00a65e51107ef066dc97446749b7176e9bb0be18
mauricio <jourdanmauricio@gmail.com>
2023-05-14 05:55:34 -0300

    Fix slugs product. Add normalize url

--- src/utils/functions/normalizeUrl.js
+++ src/utils/functions/normalizeUrl.js
@@ -1,7 +1,17 @@
 export function normalizeUrl(str) {
+  console.log(
+    'URL',
+    str
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replaceAll(' ', '-')
+      .toLowerCase()
+  );
   return str
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
     .replaceAll(' ', '-')
+    .replaceAll('/', '-')
+    .replaceAll('.', '-')
     .toLowerCase();
 }
